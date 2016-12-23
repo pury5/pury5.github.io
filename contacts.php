321aeb0a8dfb101638e874edc20a8dd6f38572cf
Roman Logvynuk <mlogvynuk@gmail.com>
2016-12-23 15:23:10 +0200

    update

--- contacts.php
+++ contacts.php
@@ -1,22 +1,21 @@
 <?php
 $name = $_POST['name'];
 $email = $_POST['email'];
-$subject = $_POST['subject'];
-$message = $_POST['message'];
+$message = isset($_POST['message']) ? $_POST['message'] : null;
 $from = 'From: Test';
 $to = 'mlogvynuk@gmail.com';
-$subject = $_POST['subject'];
+$subject = 'hello';
 $human = $_POST['human'];
 
 $body = "From: $name \n E-Mail: $email \n Subject: $subject \n Message: $message";
 
 if($_POST['submit'] && $human == '4') {
-    if(mail($to, $subject, $body, $from)) {
-        echo '<p>Your message has been sent</p>'
+    if(mail($to, $body, $from)) {
+        echo '<p>Your message has been sent</p>';
     }else{
-        echo '<p>Something went wrong, go back and try again!</p>'
+        echo '<p>Something went wrong, go back and try again!</p>';
     }
 }else if ($_POST['submit'] && $human !='4') {
-    echo '<p>You answered the anti-spam question incorrectly!</p>'
+    echo '<p>You answered the anti-spam question incorrectly!</p>';
 }
 ?>
