a5e45953d251bfb4ff6e1be7734722666049dcf8
Roman Logvynuk <mlogvynuk@gmail.com>
2016-12-23 04:03:10 +0200

    update

--- contacts.php
+++ contacts.php
@@ -1,22 +1,22 @@
 <?php
-$name = $_POST[&#039name&#039];
-$email = $_POST[&#039email&#039];
-$subject = $_POST[&#039subject&#039];
-$message = $_POST[&#039message&#039];
-$from = &#039From: Test&#039;
-$to = &#039mlogvynuk@gmail.com&#039;
-$subject = $_POST[&#039subject&#039];
-$human = $_POST[&#039human&#039];
+$name = $_POST['name'];
+$email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+$from = 'From: Test';
+$to = 'mlogvynuk@gmail.com';
+$subject = $_POST['subject'];
+$human = $_POST['human'];
 
-$body = &#039From: $name \n E-Mail: $email \n Subject: $subject \n Message: $message&#039;
+$body = "From: $name \n E-Mail: $email \n Subject: $subject \n Message: $message";
 
-if($_POST[&#039submit&#039] && $human == &#0394&#039) {
+if($_POST['submit'] && $human == '4') {
     if(mail($to, $subject, $body, $from)) {
-        echo &#039<p>Your message has been sent</p>&#039
+        echo '<p>Your message has been sent</p>'
     }else{
-        echo &#039<p>Something went wrong, go back and try again!</p>&#039
+        echo '<p>Something went wrong, go back and try again!</p>'
     }
-}else if ($_POST[&#039submit&#039] && $human !=&#0394&#039) {
-    echo &#039<p>You answered the anti-spam question incorrectly!</p>&#039
+}else if ($_POST['submit'] && $human !='4') {
+    echo '<p>You answered the anti-spam question incorrectly!</p>'
 }
 ?>
